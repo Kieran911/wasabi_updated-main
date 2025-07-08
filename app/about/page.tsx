6c5c2c0be9476e9c99ce0a9180905eb242fb1621
Kieran911 <arinzekieran@gmail.com>
2025-07-09 00:11:16 +0100

    updates

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,4 @@
 import { AboutAndBelief } from './components/about-belief';
-import { CuratedMenu } from './components/curated-menu';
 import { DinnerTable } from './components/dinnerTable';
 import { FeastInStyle } from './components/feast-in-style';
 import { ImagesSlider } from './components/image-slider';
